b76c33e6bc449b583ae68881217430a9d685370b
Luan Naves <luandhx@gmail.com>
2023-06-13 16:29:51 -0300

    Exercicios Academia.NET Atos

---
+++ ExerciciosRestAPI/ExerciciosRestAPI/Controllers/Exercicio1Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExerciciosRestAPI.Controllers {
+    [ApiController]
+    [Route("Exercicio1Controller")]
+    public class Exercicio1Controller : ControllerBase {
+        [HttpGet("nome")]
+        public string mostraMeuNome() {
+            return "Luan";
+        }
+        [HttpGet("idade")]
+        public int mostraMinhaIdade() {
+            return 21;
+        }
+        [HttpGet("nome/{seuNome}")]
+        public string seuNome([FromRoute] string seuNome) {
+            return seuNome;
+        }
+        [HttpGet("idade/{suaIdade}")]
+        public int suaIdade([FromRoute] int suaIdade) { 
+            return suaIdade;
+        }
+    }
+}
